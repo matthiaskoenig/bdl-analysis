d7d0b9db55b2d3e34752062c2125b440264f33b9
Matthias Koenig <konigmatt@googlemail.com>
2015-08-03 17:58:52 +0200

    ActB control plots generated

--- BDLanalysis/R/plots.R
+++ BDLanalysis/R/plots.R
@@ -7,12 +7,16 @@
 #'
 #' Plot the time course of a single factor, i.e. all individual data points.
 #' @export
-plot_single_factor <- function(name){
+plot_single_factor <- function(name, path=NULL, k=NULL){
   dA <- BDLdata[, name]
   
   # search if probe info is available
   info <- ProbeInformation(geneId=name)  
   
+  if (! is.null(path)){
+    fname <- paste(path, "/factors/", sprintf("%03d", k), "_", name, ".png", sep="")
+    png(filename=fname, width=1600, height=800, res=150)
+  }
   par(mfrow=c(1,2))
   
   # [A] plot against time
@@ -27,10 +31,9 @@ plot_single_factor <- function(name){
          labels=info$Protein.name, cex=0.8)
   }
   # add mean
-  mean.time <- as.numeric(levels(as.factor(BDLsamples$time)))
-  Nt <- length(mean.time)
-  points(mean.time, BDLmean[, name], col="red", pch=15)
-  lines(mean.time, BDLmean[, name], col="red")
+  Nt <- length(BDLmean.time)
+  points(BDLmean.time, BDLmean[, name], col="red", pch=15)
+  lines(BDLmean.time, BDLmean[, name], col="red")
   
   # [B] plot as factor (non-equidistant time points)
   plot(BDLsamples$time_fac, dA, xlab="time", ylab=name, main=name, col=rgb(0.5,0.5,0.5, 0.4),
@@ -42,6 +45,9 @@ plot_single_factor <- function(name){
   lines(1:Nt, BDLmean[, name], col="red")
   
   par(mfrow=c(1,1))
+  if (!is.null(path)){
+    dev.off()
+  }
 }
 
 #' Creates single factor plots of all factors.
@@ -49,17 +55,14 @@ plot_single_factor <- function(name){
 #' Iterates over all factor variables in the BDLdata frame and creates
 #' the individual factor plots.
 #' @export 
-plot_all_factors <- function(){
+plot_all_factors <- function(path){
   
   factors <- colnames(BDLdata)
   Nf = length(factors)
   for (k in 1:Nf){
     cat(sprintf("%s / %s\n", k, Nf))
     name <- factors[k]
-    # fname <- paste("../results/factors/", sprintf("%03d", k), "_", name, ".png", sep="")
-    # png(filename=fname, width=options$width, height=options$height, res=options$res)
-    plot_single_factor(name)
-    # dev.off()
+    plot_single_factor(name, path, k)
   }  
 }
 
@@ -67,49 +70,53 @@ plot_all_factors <- function(){
 #' 
 #' Plots the mean time course and individual samples.
 #' @export
-f_single_plot <- function(name_A){
-  dA <- data[[name_A]]
-  dA.mean <- dmean[[name_A]]
-  plot(samples$time_fac, dA, xlab="time", ylab=name_A, main=name_A, col=rgb(0.5,0.5,0.5, 0.4),
+plot_single <- function(name_A){
+  dA <- BDLdata[[name_A]]
+  dA.mean <- BDLmean[[name_A]]
+  plot(BDLsamples$time_fac, dA, xlab="time", ylab=name_A, main=name_A, col=rgb(0.5,0.5,0.5, 0.4),
        ylim=c(0, max(dA, na.rm=TRUE)*1.1))
-  points(samples$time_fac, dA, col="black")
-  points(samples$time_fac, dA, col=rgb(0,0,1,0.6), pch=16)
+  points(BDLsamples$time_fac, dA, col="black")
+  points(BDLsamples$time_fac, dA, col=rgb(0,0,1,0.6), pch=16)
   # plot the repeat number
   dA.text <- dA
   dA[is.na(dA)] <- -1
   # library(calibrate)
-  textxy(samples$time_point, dA, samples$sid, col="black", cex=0.6)
+  textxy(BDLsamples$time_point, dA, BDLsamples$sid, col="black", cex=0.6)
   
-  points(1:nrow(dmean), dA.mean, col="red", pch=15)
-  lines(1:nrow(dmean), dA.mean, col="red")
+  points(1:nrow(BDLmean), dA.mean, col="red", pch=15)
+  lines(1:nrow(BDLmean), dA.mean, col="red")
 }
 
-#' Correlation plot between two factors
+
+#' Correlation plot between two factors.
 #' 
+#' Plot the two individual factors and the additional correlation plot
+#' for the data.
 #' @export
-f_cor_pair_plot <- function(name_A, name_B, single_plots=TRUE){
+plot_cor_pair <- function(name_A, name_B, single_plots=TRUE){
   if (single_plots){
     layout(matrix(c(1,1,2,3), 2, 2, byrow = TRUE))  
   }
   # correlation plot
-  dA <- data[[name_A]]
-  dB <- data[[name_B]]
+  dA <- BDLdata[[name_A]]
+  dB <- BDLdata[[name_B]]
   # dA <- dA[!is.na(dA)]
   # d <- dA[!is.na(dA)]
-  dA.mean <- dmean[[name_A]]
-  dB.mean <- dmean[[name_B]]
+  dA.mean <- BDLmean[[name_A]]
+  dB.mean <- BDLmean[[name_B]]
   
   value.max <- 1.1* c(max(dA), max(dB))
   plot(dA, dB, xlim=c(0, value.max[1]), ylim=c(0, value.max[2]), 
        main=sprintf("%s ~ %s", name_A, name_B),
        xlab=name_A, ylab=name_B)
-  textxy(dA, dB, samples$time)
+  textxy(dA, dB, BDLsamples$time)
   points(dA.mean, dB.mean, pch=16, col=rgb(0,0,1,0.8) )
   lines(dA.mean, dB.mean, col=rgb(0,0,1,0.8) )
-  textxy(dA.mean, dB.mean, dmean.time, col="blue")
+  
+  textxy(dA.mean, dB.mean, BDLmean.time, col="blue")
   abline(a=0, b=mean(dB.mean)/mean(dA.mean), col="darkgray")
   
-  text(x=0.05*value.max[1], y=0.9*value.max[2], 
+  text(x=0.05*value.max[1], y=0.9*value.max[2], cex=0.5,
        labels=sprintf("S: %1.3f\nS mean: %1.3f\nP: %1.3f\nP mean: %1.3f", 
                       cor(dA, dB, method="spearman", use="pairwise.complete.obs"),
                       cor(dA.mean, dB.mean, method="spearman", use="pairwise.complete.obs"),
@@ -118,8 +125,8 @@ f_cor_pair_plot <- function(name_A, name_B, single_plots=TRUE){
   
   # single plots
   if (single_plots){
-    f_single_plot(name_A)
-    f_single_plot(name_B)
+    plot_single(name_A)
+    plot_single(name_B)
     layout(matrix(c(1), 1, 1, byrow = TRUE))
   }
 }
--- analysis/run_analysis.R
+++ analysis/run_analysis.R
@@ -15,90 +15,15 @@
 
 rm(list=ls())
 require('BDLanalysis')
-require('reshape')
-require("calibrate")
-
-
-
-
-help(BDLanalysis)
-
-# Preprocess dataset
-
-
-
-
-
-
-
-#---------------------------------------------
-# Single factor visualization
-#---------------------------------------------
-# Create plot of the first factor
-plot_single_factor(1)
-
-# Creates plots of all factors in data
-plot_all_factors()
 
 
 #---------------------------------------------
 # All data heatmap
 #---------------------------------------------
 
-#--------------------------
-# Actb controls
-#--------------------------
-# Actb was measured on all Fluidigm chips and serves as quality control of the 
-# measurement/correlation analysis.
-# Use pairwise correlation plots for control.
-f_cor_pair_plot("Actb", "Por")
-
-# Actb control figure
-options <- list(width=1600, height=600, res=200)
-png(filename="../results/Actb_control.png", width=options$width, height=options$height, res=options$res)
-par(mfrow=c(1,3))
-f_cor_pair_plot("Actb", "Actb.x", single_plots=FALSE)
-f_cor_pair_plot("Actb", "Actb.y", single_plots=FALSE)
-f_cor_pair_plot("Actb.x", "Actb.y", single_plots=FALSE)
-par(mfrow=c(1,1))
-dev.off()
-
-# calculate the correlations
-f_cor_pair_plot("Actb", "Actb.x")
-f_cor_pair_plot("Actb", "Actb.y")
-f_cor_pair_plot("Actb.x", "Actb.y")
-
-cat('Actb Spearman : individual points\n')
-actb.spearman <- cor(data.frame(Actb=data$Actb, 
-                                Actb.x=data$Actb.x, 
-                                Actb.y=data$Actb.y), method="spearman")
-print(actb.spearman)
-cat('Actb Spearman : mean\n')
-actb.spearman.mean <- cor(data.frame(Actb=dmean$Actb, 
-                                     Actb.x=dmean$Actb.x, 
-                                     Actb.y=dmean$Actb.y), method="spearman")
-print(actb.spearman.mean)
-cat('Actb Pearson : individual points\n')
-actb.pearson <- cor(data.frame(Actb=data$Actb, 
-                               Actb.x=data$Actb.x, 
-                               Actb.y=data$Actb.y), method="pearson")
-print(actb.pearson)
-cat('Actb Pearson : mean\n')
-actb.pearson.mean <- cor(data.frame(Actb=dmean$Actb, 
-                                    Actb.x=dmean$Actb.x, 
-                                    Actb.y=dmean$Actb.y), method="pearson")
-print(actb.pearson.mean)
-
-f_cor_pair_plot("Actb", "Por")
 
 
 
-
-
-
-# plot options
-# @export
-# options = list(width=1600, height=800, res=150)
 
 
 

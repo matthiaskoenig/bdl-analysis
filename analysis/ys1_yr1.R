032943cd87eee4a8f7ee611e0fa2d3306a0cd9d4
Matthias Koenig <konigmatt@googlemail.com>
2015-08-04 11:49:30 +0200

    old changes in ys1 and yr1

--- analysis/ys1_yr1.R
+++ analysis/ys1_yr1.R
@@ -1,13 +1,16 @@
 ##############################################################################################
 #
-#    YS1 and YR1 adapted
+#    YS1, YR1, YS2, YR2 implementation
 #
 #    Matthias Koenig
-#    2015-07-13
+#    2015-07-22
 #
-#    YS1 and YR1 implementation based on
-#   "A modfied correlation coefficient based similarity measure for clustering
+#    Correlation score for time course data based on 
+#    "A modfied correlation coefficient based similarity measure for clustering
 #     time-course gene expression data" [Son2007]
+#
+#    For recent reviews of correlation scores for gene expression data look
+#    Ja
 #
 #     ys1 and yr1 are a weighted score including contributions of classical correlation score and
 #     shape of the time course profile, namely the slope between measurements and the minimal
@@ -23,6 +26,7 @@
 #
 ##############################################################################################
 
+# TODO: implement how to handle small fluctuations in slope.
 
 # --------------------------------------------------------------------------------------------
 # Classical correlation component (Spearman/Pearson)
@@ -380,9 +384,164 @@ yr1.df <- function(data, time_pts, w1=0.50, w2=0.25, w3=0.25, use="pairwise.comp
   return(cor.mat)
 }
 
+
+# Calculate ys1 matrix for a given data frame.
+# Every column of the data.frame is a single measurement (or mean) of a single factor for
+# the provided time points. 
+# Use this function to calculate the correlation matrix on the mean data.
+ys1.df <- function(data, time_pts, w1=0.50, w2=0.25, w3=0.25, use="pairwise.complete.obs"){
+  N <- ncol(data)
+  value.mat <- matrix(NA, nrow=N, ncol=N)
+  colnames(value.mat) <- names(data)
+  rownames(value.mat) <- names(data)
+  S_star.mat <- value.mat
+  A.mat <- value.mat
+  M.mat <- value.mat
+  
+  for (k in 1:N){
+    for (i in 1:N){
+      # calculate the simple score pairwise between all factors
+      res <- ys1(data[,k], data[,i], time_pts, w1=w1, w2=w2, w3=w3, use=use)
+      value.mat[k,i] = res$value
+      S_star.mat[k,i] = res$S_star
+      A.mat[k,i] = res$A
+      M.mat[k,i] = res$M
+    }
+  }
+  return(list(value=value.mat, S_star=S_star.mat, A=A.mat, M=M.mat))
+}
+
+ys2.df <- function(data, time_pts, w1=0.50, w2=0.25, w3=0.25, use="pairwise.complete.obs"){
+  N <- ncol(data)
+  value.mat <- matrix(NA, nrow=N, ncol=N)
+  colnames(value.mat) <- names(data)
+  rownames(value.mat) <- names(data)
+  S_star.mat <- value.mat
+  A_star.mat <- value.mat
+  A_star2.mat <- value.mat
+  M_star.mat <- value.mat
+  M_star2.mat <- value.mat
+  for (k in 1:N){
+    for (i in 1:N){
+      # calculate the simple score pairwise between all factors
+      res <- ys2(data[,k], data[,i], time_pts, w1=w1, w2=w2, w3=w3, use=use)
+      value.mat[k,i] = res$value
+      S_star.mat[k,i] = res$S_star
+      A_star.mat[k,i] = res$A_star
+      A_star2.mat[k,i] = res$A_star2
+      M_star.mat[k,i] = res$M_star
+      M_star2.mat[k,i] = res$M_star2
+    }
+  }
+  return(list(value=value.mat, S_star=S_star.mat, 
+              A_star=A_star.mat, A_star2=A_star2.mat, 
+              M_star=M_star.mat, M_star2=M_star2.mat))
+}
+
+
+# Calculate ys1 for factors provided as list of matrices with size [Nt, Nr].
+ys1.timecourse.df <- function(data_list, time_pts, w1=0.50, w2=0.25, w3=0.25, use="pairwise.complete.obs"){
+  Nf <- length(data_list)
+  value.mat <- matrix(NA, nrow=Nf, ncol=Nf)
+  colnames(value.mat) <- names(data_list)
+  rownames(value.mat) <- names(data_list)
+  S_star.mat <- value.mat
+  A.mat <- value.mat
+  M.mat <- value.mat
+  
+  for (k in 1:Nf){
+    for (i in 1:Nf){
+      cat(sprintf("[%s, %s]\n", k, i))
+      res <- ys1.timecourse(data_list[[k]], data_list[[i]], time_pts, w1=w1, w2=w2, w3=w3, use=use)
+      value.mat[k,i] = res$value
+      S_star.mat[k,i] = res$S_star
+      A.mat[k,i] = res$A
+      M.mat[k,i] = res$M
+    }
+  }
+  return(list(value=value.mat, S_star=S_star.mat, A=A.mat, M=M.mat))
+}
+
+
+# Calculate ys1 for factors provided as list of matrices with size [Nt, Nr].
+ys1.timepoints.df <- function(data_list, time_pts, w1=0.50, w2=0.25, w3=0.25, use="pairwise.complete.obs"){
+  Nf <- length(data_list)
+  value.mat <- matrix(NA, nrow=Nf, ncol=Nf)
+  colnames(value.mat) <- names(data_list)
+  rownames(value.mat) <- names(data_list)
+  S_star.mat <- value.mat
+  A.mat <- value.mat
+  M.mat <- value.mat
+  
+  for (k in 1:Nf){
+    for (i in 1:Nf){
+      cat(sprintf("[%s, %s]\n", k, i))
+      res <- ys1.timepoints(data_list[[k]], data_list[[i]], time_pts, w1=w1, w2=w2, w3=w3, use=use)
+      value.mat[k,i] = res$value
+      S_star.mat[k,i] = res$S_star
+      A.mat[k,i] = res$A
+    }
+  }
+  return(list(value=value.mat, S_star=S_star.mat, A=A.mat))
+}
+
+# 
+# ys1.timepoints(data_list[[1]], data_list[[2]], time_pts=dmean.time)
+# ys1.timepoints(data_list[[136]], data_list[[42]], time_pts=dmean.time)
+# 
+# ys1.timecourse(data_list[[1]], data_list[[2]], time_pts=dmean.time)
+# ys1.timecourse.df(data_list[1:2], time_pts=dmean.time)
+# 
+# data_list[1:2]
+# name_A = "Ppara"   # [[1]]
+# name_B = "Cyp3a11" # [[2]]
+# f_cor_pair_plot(name_A, name_B)
+# ys1(a=dmean[[name_A]], b=dmean[[name_B]], time_pts=dmean.time, w1=0.25, w2=0.5, w3=0.25)
+# ys2(a=dmean[[name_A]], b=dmean[[name_B]], time_pts=dmean.time, w1=0.25, w2=0.5, w3=0.25)
+# 
+# # 136 Actb
+# # 42 Actb.x
+# # 89 Actb.y
+# 
+# ys1.timecourse.df(data_list[c(42,89,136)], time_pts=dmean.time)
+# 
+# ys1.timecourse.df(data_list[c(1,154)], time_pts=dmean.time)
+# 
+# #--------------------------------------------------------------------------------------------------------
+# # Example calculations on BDL dataset.
+# 
+# # calculate ys1 on mean timecourse data
+# ys1.res <- ys1.df(dmean, dmean.time)
+# 
+# source("ys1_yr1.R")  # definition of ys1 and yr1
+# 
+# ys2.res <- ys2.df(dmean, dmean.time)
+# f_corrplot("cor.ys2", data=ys2.res$value, order="original")
+# f_corrplot("cor.ys2-scaled", data=2*(ys2.res$value-0.5), order="original")
+# f_corrplot("cor.ys2", data=ys2.res$value, order="hclust")
+# f_corrplot("cor.ys2-scaled", data=2*(ys2.res$value-0.5), order="hclust")
+# options$height=1600
+# options$width=1600
+# summary(ys2.res$value)
+# 
+
+
+
+
+
+
+
+
+
 # -----------------------------------------------------------------------
 # Implementation Tests from [Son2008]
 # -----------------------------------------------------------------------
+# test function
+test_res <- function(y, y_expected, digits=3){
+  stopifnot(round(y, digits=digits) == y_expected)
+}
+
+# example data from Son2008
 time_pts <- seq(0,4)
 x1 <- c(2,3,6,4,7)
 x2 <- c(1,2,3,5,3)
@@ -400,11 +559,6 @@ plot(time_pts, y1, col="blue", pch=16, type="o", ylim=c(0, max(max(y1), max(y2))
 points(time_pts, y2, col="red", pch=15, type="o")
 par(mfrow=c(1,1))
 
-# ys1
-test_res <- function(y, y_expected, digits=3){
-  stopifnot(round(y, digits=digits) == y_expected)
-}
-
 # correlation values
 test_res(cor(x=x1,y=x2, method='spearman'), 0.667)
 test_res(cor(x=x1,y=x2, method='pearson'),  0.439)
@@ -419,6 +573,7 @@ rm(test_res, x1, x2, y1, y2, time_pts)
 
 # TODO: implement the tests for ys2
 # -----------------------------------------------------------------------
+
 
 
 
